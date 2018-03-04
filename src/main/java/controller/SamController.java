2dc43123206a52b145f8c1df7697a8bf5138593e
yy <13425158179@126.com>
2018-03-04 18:52:54 +0800

    添加新的控制器

---
+++ src/main/java/controller/SamController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO 类的描述
+ *
+ * @author liuyanli
+ * @createTime 2018-03-02 14:41:28
+ */
+@EnableAutoConfiguration
+@RestController
+public class SamController {
+    @RequestMapping("/yy")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SamController.class, args);
+    }
+}
